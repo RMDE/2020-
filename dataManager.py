9bcf8db128ef947a42d9055ed1e897d94dcb0c20
unknown <435869851@qq.com>
2020-08-30 09:59:50 +0800

    图像预处理，增加数据量

--- dataManager.py
+++ dataManager.py
@@ -1,6 +1,3 @@
-'''
-用途：用于yolov3数据预处理，以增加训练数据量
-'''
 import os                           # os
 from collections import namedtuple  # 集合定义
 
@@ -52,19 +49,74 @@ def horizon_flip(img,lab,p=0.5):
 
 
 # 随机裁剪
-def random_crop(img,label,p=0.5,min=40,max=68): # (min,max)->range of width and height
+def random_crop(img,lab,p=0.5,min=40,max=68): # (min,max)->range of width and height
     if np.random.random()<p:
-        width = np.random.randint(min,max)
-        height = np.random.randint(min,max)
-        point_x = np.random.randint(img.shape[1]/4,(img.shape[1]/4)*3)
-        point_y = np.random.randint(img.shape[0]/4,(img.shape[0]/4)*3)
-        img_region = img[point_x-width:point_x+width,point_y-height:point_y+height,:]
-        label_region = label[point_x-width:point_x+width,point_y-height:point_y+height,:]
-        if len(img_region)>0 and len(label_region)>0:
-            print(len(img_region),len(label_region))
-            img_region = cv2.resize(img_region,(img.shape[1],img.shape[0]))
-            label_region = cv2.resize(label_region,(label.shape[1],label.shape[0]))
-            return img_region,label_region,True
+        xmin = []
+        ymin = []
+        xmax = []
+        ymax = []
+        new_width = np.random.randint(min,max)
+        new_height = np.random.randint(min,max)
+        point_x = np.random.randint(0,(img.shape[1]/2))
+        point_y = np.random.randint(0,(img.shape[0]/2))
+        if new_width+point_x > img.shape[1]:
+            new_width = img.shape[1]-point_x
+        if new_height+point_y > img.shape[0]:
+            new_height = img.shape[0]-point_y
+        print(new_width,new_height)
+        print(point_x,point_y)
+        img_region = img[point_y:point_y+new_height,point_x:point_x+new_width,:]
+        print(img.shape[1],img.shape[0],img_region.shape[1],img_region.shape[0])
+        root = lab.documentElement
+        tags = root.getElementsByTagName('width')
+        tags[0].firstChild.data = str(new_width)
+        tags = root.getElementsByTagName('height')
+        tags[0].firstChild.data = str(new_height)
+        tags = root.getElementsByTagName('xmin')
+        for x in tags:
+            xmin.append(x)
+        tags = root.getElementsByTagName('ymin')
+        for x in tags:
+            ymin.append(x)
+        tags = root.getElementsByTagName('xmax')
+        for x in tags:
+            xmax.append(x)
+        tags = root.getElementsByTagName('ymax')
+        for x in tags:
+            ymax.append(x)
+        for i in range(len(xmin)):
+            left = int(xmin[i].firstChild.data)
+            right = int(xmax[i].firstChild.data)
+            top = int(ymin[i].firstChild.data)
+            bottom = int(ymax[i].firstChild.data)
+            print("lrtb:",left,right,top,bottom)
+            a = []
+            a.append(left-point_x)
+            a.append(right-point_x)
+            b = []
+            b.append(top-point_y)
+            b.append(bottom-point_y)
+            print("rse:",a,b)
+            for x in range(len(a)):
+                if a[x] < 0:
+                    a[x] = 0
+                elif a[x] > new_width:
+                    a[x] = new_width
+
+            for x in range(len(b)):
+                if b[x] < 0:
+                    b[x] = 0
+                elif b[x] > new_height:
+                    b[x] = new_height
+            xmin[i].firstChild.data = str(a[0])
+            xmax[i].firstChild.data = str(a[1])
+            ymin[i].firstChild.data = str(b[0])
+            ymax[i].firstChild.data = str(b[1])
+        nodes = root.getElementsByTagName('object')
+        for i in range(len(nodes)):
+            if xmin[i].firstChild.data == xmax[i].firstChild.data or ymin[i].firstChild.data == ymax[i].firstChild.data:
+                root.removeChild(nodes[i])
+        return img_region,lab,True
     return [],[],False
 
 
@@ -88,17 +140,23 @@ def handle_data():
         if flag == True:
             ResImg.append(fimg)
             ResLab.append(flab)
+        for num in range(4):
+            lab = parse(os.path.join(curDir, labels[i]))
+            fimg,flab,flag = random_crop(img,lab,1,min=2000,max=3000)
+            if flag == True:
+                ResImg.append(fimg)
+                ResLab.append(flab)
 
     for i in range(len(ResImg)):
-        cv2.imwrite("./data/fimg/IMG_0"+str(661+i)+".png",ResImg[i])
-        f = open("./data/flab/LABEL_0"+str(661+i)+".xml",'w')
+        cv2.imwrite("./data/fimg/IMG_0"+str(761+i)+".png",ResImg[i])
+        f = open("./data/flab/LABEL_0"+str(761+i)+".xml",'w')
         root = ResLab[i].documentElement
         tags = root.getElementsByTagName('folder')
         tags[0].firstChild.data = "flab"
         tags = root.getElementsByTagName('filename')
         tags[0].firstChild.data = "LABEL_0"+str(661+i)+".xml"
         tags = root.getElementsByTagName('path')
-        tags[0].firstChild.data = "E:\\yolov3\\data\\flab\\LABEL_0"+str(661+i)+".xml"
+        tags[0].firstChild.data = "E:\\yolov3\\data\\flab\\LABEL_0"+str(761+i)+".xml"
         ResLab[i].writexml(f)   
 
     print("Handle the train data success, length:", len(imgs))
